4a7ab742b4e46b20dbb40dcaaf83b62d7f328c64
NathanCheshire <nathan.vincent.2.718@gmail.com>
2020-12-18 12:39:47 -0600

    ConsoleFrame work

--- src/ConsoleFrame.java
+++ src/ConsoleFrame.java
@@ -1,29 +1,29 @@
+import com.cyder.enums.ConsoleDirection;
 import com.cyder.enums.Direction;
+import com.cyder.handler.ErrorHandler;
 import com.cyder.ui.CyderFrame;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class ConsoleFrame extends CyderFrame {
 
-    //current user vars
-    private static String UUID;
-    private static String username;
-    private static String userColor;
-    private static String userFont;
-
-    //background vars
-
-    //command list vars
-
-
     public ConsoleFrame(int width, int height) {
         super(width, height);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 
     }
 
-    public static void setUUID() {
-        //this will setuuid and set username also
+    private static String UUID;
+
+    public static void setUUID(String uuid) {
+        UUID = uuid;
     }
 
     public static String getUUID() {
@@ -31,7 +31,15 @@ public class ConsoleFrame extends CyderFrame {
     }
 
     public static String getUsername() {
-        return username;
+        return null;
+    }
+
+    private static Font getUserFont() {
+        return null;
+    }
+
+    private static Color getUserColor() {
+        return null;
     }
 
     public static void resizeBackgrounds() {
@@ -39,57 +47,119 @@ public class ConsoleFrame extends CyderFrame {
     }
 
     public static void initBackgrounds() {
+        try {
+            File dir = new File("src/users/" + getUUID() + "/Backgrounds");
+            FilenameFilter PNGFilter = (dir1, filename) -> filename.endsWith(".png");
 
+            backgroundFiles = new LinkedList<>(Arrays.asList(dir.listFiles(PNGFilter)));
+
+            if (backgroundFiles.size() == 0)
+                backgroundFiles = new LinkedList<>();
+                backgroundFiles.add(new File("src/com/cyder/sys/pictures/Bobby.png"));
+        }
+
+        catch (Exception ex) {
+            ErrorHandler.handle(ex);
+        }
     }
+
+    private static LinkedList<File> backgroundFiles;
 
     public static File[] getBackgrounds() {
         return null;
     }
 
+    private static File backgroundFile;
+    private static int backgroundIndex;
+
     public static File getCurrentBackgroundFile() {
-        return null;
+        backgroundFile = backgroundFiles.get(backgroundIndex);
+
+        return backgroundFile;
     }
 
+    private static ImageIcon backgroundImageIcon;
+
     public static ImageIcon getCurrentBackgroundImageIcon() {
-        return null;
+        try {
+            File f = getCurrentBackgroundFile();
+
+            backgroundImageIcon = new ImageIcon(ImageIO.read(f));
+        }
+
+        catch (Exception e) {
+            ErrorHandler.handle(e);
+        }
+
+        return backgroundImageIcon;
     }
 
     private static Direction lastSlideDirection = Direction.LEFT;
 
     public static void switchBackground() {
+        if (!(backgroundFiles.size() > backgroundIndex + 1 && backgroundFiles.size() > 1))
+            return;
+
         switch (lastSlideDirection) {
             case LEFT:
+
+
 
                 break;
 
             case RIGHT:
+
+
 
                 break;
         }
     }
 
     public static int getBackgroundWidth() {
-        return 0;
+        return getCurrentBackgroundImageIcon().getIconWidth();
     }
 
     public static int getBackgroundHeight() {
-        return 0;
+        return getCurrentBackgroundImageIcon().getIconHeight();
     }
 
-    public static void enableClock() {
+    private static boolean consoleClockEnabled;
+
+    public static void setConsoleClock(Boolean enable) {
+        consoleClockEnabled = enable;
+
+        if (enable) {
+            //set console clock visible
+            //start executor to update console clcok
+        }
+    }
+
+    public static boolean isConsoleClockEnabled() {
+        return consoleClockEnabled;
+    }
+
+    private static ConsoleDirection consoleDir = ConsoleDirection.UP;
+
+    public static void setConsoleDirection(ConsoleDirection conDir) {
+        consoleDir = conDir;
+    }
+
+    public static ConsoleDirection getConsoleDirection() {
+        return consoleDir;
+    }
+
+    public static void rotateConsole(ConsoleDirection dir) {
 
     }
 
-    public static void disableClock() {
+    private static boolean fullscreen = false;
+
+    public static void setFullscreen(Boolean enable) {
 
     }
 
-    public static void rotateConsole() {
-
-    }
-
-    public static void toggleFullScreen() {
-
+    public static boolean isFullscreen() {
+        return fullscreen;
     }
 
     public static void barrelRoll() {
