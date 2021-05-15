6ceb8f3040b2e94910b178fa2eaf2d58a47dc19c
NathanCheshire <nathan.vincent.2.718@gmail.com>
2021-05-14 22:36:55 -0500

    add todos (I keep coming up with new great ideas)

--- src/com/cyder/handler/InputHandler.java
+++ src/com/cyder/handler/InputHandler.java
@@ -5,4 +5,18 @@ public class InputHandler {
     // and direct the resulting flow and output
 
     //todo make menu smaller and font whatever you have for nathan user right now: Agency FB
+
+    //todo put a dollar before where you enter text
+    //todo combine text area and input field into one
+    //todo startup routine
+    //todo context engine that links with inputhandler
+    // you can add more links and hooks to inputhandler
+    //todo if cyder hasn't started in a while, say welcome back $username, did you miss me?
+    // or some variation of that
+    //todo implement contextengine
+    //todo engines package within handler
+
+    //todo look into hault method from Runtime class
+
+    //todo rick and morty references
 }
