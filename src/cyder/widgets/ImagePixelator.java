c8396e43f1dc2984b05494595e1d14aff403916b
NathanCheshire <nathan.vincent.2.718@gmail.com>
2021-07-09 12:30:30 -0500

    finish ImagePixelator

--- src/cyder/widgets/ImagePixelator.java
+++ src/cyder/widgets/ImagePixelator.java
@@ -5,17 +5,26 @@ import cyder.consts.CyderFonts;
 import cyder.consts.CyderImages;
 import cyder.handler.ErrorHandler;
 import cyder.ui.*;
+import cyder.utilities.GetterUtil;
+import cyder.utilities.ImageUtil;
 import cyder.utilities.StringUtil;
+import cyder.utilities.SystemUtil;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
+import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 import java.io.File;
 
 public class ImagePixelator {
     private ImageIcon displayIcon;
+    private ImageIcon originalIcon;
+    private File currentFile;
+    private JLabel previewLabel;
+    CyderTextField integerField;
 
     public ImagePixelator(File startPNG) {
         CyderFrame pixelFrame = new CyderFrame(800,800, CyderImages.defaultBackground);
@@ -33,17 +42,63 @@ public class ImagePixelator {
         chooseImage.setBounds(50,100,200,40);
         pixelFrame.getContentPane().add(chooseImage);
         chooseImage.addActionListener(e -> {
+            try {
+                new Thread(() -> {
+                    try {
+                        File temp = new GetterUtil().getFile("Choose file to resize");
 
+                        if (temp != null && temp.getName().toLowerCase().endsWith(".png")) {
+                            currentFile = temp;
+
+                            displayIcon = checkImage(temp);
+                            originalIcon = new ImageIcon(ImageIO.read(temp));
+                            previewLabel.setIcon(displayIcon);
+                            integerField.setCharLimit(String.valueOf(originalIcon.getIconWidth()).length());
+                            previewLabel.revalidate();
+                            pixelFrame.revalidate();
+                            previewLabel.repaint();
+                            pixelFrame.repaint();
+                        } else if (temp != null && !StringUtil.getExtension(temp).equalsIgnoreCase(".png")) {
+                            currentFile = null;
+                            displayIcon = null;
+                        }
+                    } catch (Exception ex) {
+                        ErrorHandler.handle(ex);
+                    }
+                }, "wait thread for GetterUtil().getFile()").start();
+            }
+
+            catch (Exception ex) {
+                ErrorHandler.handle(ex);
+            }
         });
 
-        CyderTextField integerField = new CyderTextField(0);
+        integerField = new CyderTextField(0);
+        integerField.setRegexMatcher("[0-9]*");
         integerField.setBounds(300,100,200,40);
         integerField.setToolTipText("How many old pixels should be combined into a new pixel?");
         pixelFrame.getContentPane().add(integerField);
         integerField.addKeyListener(new KeyAdapter() {
             @Override
-            public void keyTyped(KeyEvent e) {
+            public void keyReleased(KeyEvent e) {
+                try {
+                    int pixelSize = 1;
 
+                    if (integerField.getText() != null && integerField.getText().length() > 0) {
+                        if (Integer.parseInt(integerField.getText()) == 0) {
+                            pixelSize = 1;
+                        } else {
+                            pixelSize = Integer.parseInt(integerField.getText());
+                        }
+                    }
+
+                    displayIcon = new ImageIcon(ImageUtil.pixelate(checkImageBi(currentFile),pixelSize));
+                    previewLabel.setIcon(displayIcon);
+                    previewLabel.revalidate();
+                    pixelFrame.revalidate();
+                } catch (Exception ex) {
+                    ErrorHandler.handle(ex);
+                }
             }
         });
 
@@ -52,10 +107,37 @@ public class ImagePixelator {
         approveImage.setBounds(800 - 50 - 200,100,200,40);
         pixelFrame.getContentPane().add(approveImage);
         approveImage.addActionListener(e -> {
+            if (integerField.getText() != null && integerField.getText().length() > 0) {
+                int pixel = Integer.parseInt(integerField.getText());
 
+                if (pixel > 1) {
+                    try {
+                        System.out.println("Save " + currentFile.getAbsolutePath() + " to downloads after reisizing" +
+                                " to a pixel size of " + pixel);
+
+                        BufferedImage saveImage = ImageUtil.pixelate(ImageIO.read(currentFile), pixel);
+                        String saveName = StringUtil.getFilename(currentFile) +
+                                 "_Pixelated_Pixel_Size_" + pixel + ".png";
+                        File saveFile = new File("c:/users/"
+                                + SystemUtil.getWindowsUsername() + "/downloads/" + saveName);
+
+                        ImageIO.write(saveImage, "png", saveFile);
+
+                        displayIcon = null;
+                        originalIcon = null;
+                        currentFile = null;
+                        previewLabel.setIcon(null);
+                        integerField.setText("");
+
+                        pixelFrame.notify("Successfully saved pixelated image to your downloads folder");
+                    } catch (Exception ex) {
+                        ErrorHandler.handle(ex);
+                    }
+                }
+            }
         });
 
-        JLabel previewLabel = new JLabel();
+        previewLabel = new JLabel();
         previewLabel.setBounds(50,170, 800 - 100, 610);
         previewLabel.setBorder(new LineBorder(CyderColors.navy, 5, false));
         pixelFrame.getContentPane().add(previewLabel);
@@ -65,12 +147,58 @@ public class ImagePixelator {
 
         if (startPNG != null && StringUtil.getExtension(startPNG).equalsIgnoreCase(".png")) {
             try {
-                displayIcon = new ImageIcon(ImageIO.read(startPNG));
+                currentFile = startPNG;
+                displayIcon = checkImage(startPNG);
                 previewLabel.setIcon(displayIcon);
                 previewLabel.revalidate();
+                pixelFrame.revalidate();
+                originalIcon = new ImageIcon(ImageIO.read(startPNG));
+                integerField.setCharLimit(String.valueOf(originalIcon.getIconWidth()).length());
             } catch (Exception e) {
                 ErrorHandler.handle(e);
             }
         }
+    }
+
+    private ImageIcon checkImage(File icon) {
+        ImageIcon ret = null;
+
+        try {
+            BufferedImage resizedImg = new BufferedImage(700, 610, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2 = resizedImg.createGraphics();
+
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+            g2.drawImage(ImageIO.read(icon), 0, 0, 700, 610, null);
+            g2.dispose();
+
+            ret = new ImageIcon(resizedImg);
+        } catch (Exception e) {
+            ErrorHandler.handle(e);
+        } finally {
+            return ret;
+        }
+    }
+
+    private BufferedImage checkImageBi(File icon) {
+        BufferedImage ret = null;
+
+        try {
+            BufferedImage resizedImg = new BufferedImage(700, 610, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2 = resizedImg.createGraphics();
+
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+            g2.drawImage(ImageIO.read(icon), 0, 0, 700, 610, null);
+            g2.dispose();
+
+            ret = resizedImg;
+        } catch (Exception e) {
+            ErrorHandler.handle(e);
+        } finally {
+            return ret;
+        }
+    }
+
+    private double getAspectRatio(ImageIcon im) {
+        return ((double) im.getIconWidth() / (double) im.getIconHeight());
     }
 }
