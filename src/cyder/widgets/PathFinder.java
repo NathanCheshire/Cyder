6c425d280e853659b2a55d905aa721382c2f567b
Nathan <nathan.vincent.2.718@gmail.com>
2021-08-02 23:14:14 -0500

    add adding walls, setting start, setting end, and drawing of all

--- src/cyder/widgets/PathFinder.java
+++ src/cyder/widgets/PathFinder.java
@@ -2,17 +2,35 @@ package cyder.widgets;
 
 import cyder.consts.CyderColors;
 import cyder.ui.ConsoleFrame;
+import cyder.ui.CyderCheckBox;
 import cyder.ui.CyderFrame;
+import cyder.ui.CyderLabel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.LinkedList;
 
 public class PathFinder {
-    private static double squareLen = 25;
+    private static int squareLen = 30;
+    private static int numSquares;
     private static JLabel gridLabel;
+    private static CyderCheckBox setStartBox;
+    private static CyderCheckBox setEndBox;
+    private static CyderFrame pathFindingFrame;
+
+    private static Node start;
+    private static Node end;
+    private static LinkedList<Node> walls;
 
     public static void showGUI() {
-        CyderFrame pathFindingFrame = new CyderFrame(1000,1000);
+        if (pathFindingFrame != null)
+            pathFindingFrame.closeAnimation();
+
+        walls = new LinkedList<>();
+
+        pathFindingFrame = new CyderFrame(1000,1000);
         pathFindingFrame.setTitle("Path finding visualizer");
 
         gridLabel = new JLabel() {
@@ -25,41 +43,227 @@ public class PathFinder {
                     g2d.setColor(CyderColors.navy);
                     g2d.setStroke(new BasicStroke(2));
 
-                    //horizontal lines
-                    for (int y = 0 ; y < gridLabel.getHeight() ; y += Math.floor(gridLabel.getHeight() / squareLen) - 1) {
-                        g2d.drawLine(1, y + 1, gridLabel.getWidth() - 1, y + 1);
+                    int labelWidth = gridLabel.getWidth();
+                    int labelHeight = gridLabel.getHeight();
+
+                    numSquares = (int) (Math.floor(labelWidth / squareLen));
+                    int drawTo = (int) ((Math.floor(labelWidth / squareLen)) * squareLen);
+
+                    for (int x = 1 ; x <= drawTo - 2 ; x += squareLen) {
+                        g2d.drawLine(x, 1, x, drawTo - 2);
                     }
 
-                    //vertical lines
-                    for (int x = 0 ; x < gridLabel.getWidth() ; x += Math.floor(gridLabel.getWidth() / squareLen) - 1) {
-                        g2d.drawLine(x + 1, 1, x + 1, gridLabel.getWidth() - 1);
+                    for (int y = 1 ; y <= drawTo - 2 ; y += squareLen) {
+                        g2d.drawLine(1, y, drawTo - 2, y);
                     }
 
-                    for (int x = 0 ; x < 45 ; x++) {
-                        for (int y = 0 ; y < 45 ; y++) {
-                            //g2d.fillRect(1 + squareLen * x, 1 + squareLen* y, squareLen, squareLen);
-                        }
+                    g2d.drawLine(drawTo, 1, drawTo, drawTo);
+                    g2d.drawLine(1, drawTo, drawTo, drawTo);
+
+                    if (start != null) {
+                        g2d.setColor(CyderColors.intellijPink);
+                        g2d.fillRect(2 + start.getX() * squareLen, 2 + start.getY() * squareLen,
+                                squareLen - 2, squareLen - 2);
+                        gridLabel.repaint();
+                    }
+
+                    if (end != null) {
+                        g2d.setColor(CyderColors.regularBlue);
+                        g2d.fillRect(2 + end.getX() * squareLen, 2 + end.getY() * squareLen,
+                                squareLen - 2, squareLen - 2);
+                        gridLabel.repaint();
+                    }
+
+                    for (Node wall : walls) {
+                        g2d.setColor(CyderColors.navy);
+                        g2d.fillRect(2 + wall.getX() * squareLen, 2 + wall.getY() * squareLen,
+                                squareLen - 2, squareLen - 2);
+                        gridLabel.repaint();
                     }
                 }
             }
         };
         gridLabel.addMouseWheelListener(e -> {
             if (e.isControlDown()) {
-                //up
-                if (e.getWheelRotation() == -1) {
-                    squareLen++;
+                if (e.getWheelRotation() == -1 && squareLen + 1 < 50) {
+                    squareLen += 1;
+                } else if (squareLen -1 > 0){
+                    squareLen -= 1;
+                }
+                gridLabel.repaint();
+            }
+        });
+        gridLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int x = (int) Math.floor((1 + e.getX()) / squareLen);
+                int y = (int) Math.floor((1 + e.getY()) / squareLen);
+
+                Node addNode = new Node(x, y);
+
+                if (addNode.equals(start)) {
+                    start = null;
+                } else if (addNode.equals(end)) {
+                    end = null;
+                }
+
+                if (setStartBox.isSelected()) {
+                    for (Node n : walls) {
+                        if (n.equals(addNode)) {
+                            walls.remove(n);
+                            break;
+                        }
+                    }
+
+                    start = addNode;
+                    setStartBox.setNotSelected();
+                    gridLabel.repaint();
+                } else if (setEndBox.isSelected()) {
+                    for (Node n : walls) {
+                        if (n.equals(addNode)) {
+                            walls.remove(n);
+                            break;
+                        }
+                    }
+
+                    end = addNode;
+                    setEndBox.setNotSelected();
                     gridLabel.repaint();
                 } else {
-                    squareLen--;
+                    boolean contains = false;
+                    for (Node wall : walls) {
+                        if (wall.equals(addNode)) {
+                            contains = true;
+                            break;
+                        }
+                    }
+
+                    if (!contains) {
+                        walls.add(addNode);
+                    } else {
+                        for (Node n : walls) {
+                            if (n.equals(addNode)) {
+                                walls.remove(n);
+                                break;
+                            }
+                        }
+                    }
+
                     gridLabel.repaint();
                 }
             }
         });
-        gridLabel.setSize(802,802);
+        gridLabel.setSize(800,800);
         gridLabel.setLocation(100,100);
         pathFindingFrame.getContentPane().add(gridLabel);
 
+        CyderLabel setStartLabel = new CyderLabel("Set start");
+        setStartLabel.setBounds(75,885,100,30);
+        pathFindingFrame.getContentPane().add(setStartLabel);
+
+        setStartBox = new CyderCheckBox();
+        setStartBox.setNotSelected();
+        setStartBox.setBounds(100, 920,50,50);
+        pathFindingFrame.getContentPane().add(setStartBox);
+        setStartBox.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                setEndBox.setNotSelected();
+            }
+        });
+
+        CyderLabel setEndLabel = new CyderLabel("Set end");
+        setEndLabel.setBounds(75 + 70,885,100,30);
+        pathFindingFrame.getContentPane().add(setEndLabel);
+
+        setEndBox = new CyderCheckBox();
+        setEndBox.setNotSelected();
+        setEndBox.setBounds(170, 920,50,50);
+        pathFindingFrame.getContentPane().add(setEndBox);
+        setEndBox.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                setStartBox.setNotSelected();
+            }
+        });
+
+
+        //show steps checkbox
+        //diagonal box
+        //set start
+        //set end
+        //reset button
+        //speed slider
+
         pathFindingFrame.setVisible(true);
         ConsoleFrame.getConsoleFrame().setFrameRelative(pathFindingFrame);
+    }
+
+    private static class Node {
+       private int x;
+       private int y;
+       private double h;
+       private double f;
+       private double g;
+       private Node parent;
+
+        public int getX() {
+            return x;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+
+        public double getH() {
+            return h;
+        }
+
+        public void setH(double h) {
+            this.h = h;
+        }
+
+        public double getF() {
+            return f;
+        }
+
+        public void setF(double f) {
+            this.f = f;
+        }
+
+        public double getG() {
+            return g;
+        }
+
+        public void setG(double g) {
+            this.g = g;
+        }
+
+        public Node getParent() {
+            return parent;
+        }
+
+        public void setParent(Node parent) {
+            this.parent = parent;
+        }
+
+        public Node(int x, int y) {
+           this.x = x;
+           this.y = y;
+       }
+
+       public boolean equals(Node n) {
+           return n != null && n.getX() == this.x && n.getY() == this.y;
+       }
     }
 }
