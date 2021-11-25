3d387893efa7fbda25872c6a3ace82ac3f9bc4ae
Nathan <nathan.vincent.2.718@gmail.com>
2021-11-25 13:49:34 -0600

    finish phone number regex and pass all tests first try

--- src/cyder/consts/CyderRegexPatterns.java
+++ src/cyder/consts/CyderRegexPatterns.java
@@ -8,6 +8,12 @@ public class CyderRegexPatterns {
     //zero or one number, zero or more spaces, zero or one dashes, zero or more spaces, zero or one parenthesis, 0 or 3 numbers
     // zero or one parenthesis, zero or more spaces, zero or one dashes, zero or more spaces, three numbers,
     // zero or more spaces, zero or one dashes, zero or more spaces, four numbers, ending whitespace
-    public static final String phoneNumberPattern = "";
+
+    //zero or more whitespace: \\s*
+    // escape stuff by using \\ not \ since that is special in java
+    //? before a pattern is zero or more of that char [0-9]? means zero or one of 0,1,2,...,8,9
+
+    // 1 - ( 987 ) - 345 - 5634
+    public static final String phoneNumberPattern = "\\s*[0-9]?\\s*[\\-]?\\s*[\\(]?\\s*[0-9]{0,3}\\s*[\\)]?\\s*[\\-]?\\s*[0-9]{3}\\s*[\\-]?\\s*[0-9]{4}\\s*";
     public static final String phoneNumberAreaCodeExtendedPattern = "";
 }
