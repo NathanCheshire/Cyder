3ea28866ab745d46cea888e33c7ffd855ce60876
Nathan <nathan.vincent.2.718@gmail.com>
2021-11-29 18:34:43 -0600

    add and update regex methods

--- src/cyder/consts/CyderRegexPatterns.java
+++ src/cyder/consts/CyderRegexPatterns.java
@@ -7,4 +7,41 @@ public class CyderRegexPatterns {
 
     public static final String phoneNumberPattern = "\\s*[0-9]?\\s*[\\-]?\\s*[\\(]?\\s*[0-9]{0,3}\\s*[\\)]?\\s*[\\-]?\\s*[0-9]{3}\\s*[\\-]?\\s*[0-9]{4}\\s*";
     public static final String phoneNumberAreaCodeExtendedPattern = "\\s*[0-9]{0,2}\\s*[\\-]?\\s*[\\(]?\\s*[0-9]{0,3}\\s*[\\)]?\\s*[\\-]?\\s*[0-9]{3}\\s*[\\-]?\\s*[0-9]{4}\\s*";
+
+    public String zeroOrMoreWhiteSpace() {
+        return "\\s*";
+    }
+
+    public String oneOrMoreWhiteSpace() {
+        return "\\s+";
+    }
+
+    /**
+     * Returns the regex pattern for an optional char
+     * @param chars chars enclosed in square brackets that should be optional
+     * @return the string you gave with "?" at the end basically
+     */
+    public String optionalChar(String chars) {
+        return chars + "?";
+    }
+
+    /**
+     * Returns a regex representing n successive digits
+     * @param n the number of digits to accept
+     * @return regex representing n successive digits, no more, no less
+     */
+    public String nDigits(int n) {
+        return "[0-9]{" + n + "}";
+    }
+
+    /**
+     * Same as {@code nDigits(String)} except the number of digits may be in the inclusive range of [0, n]
+     * @param n the max number of digits
+     * @return a regex representing 0 to n digits
+     */
+    public String zeroToNDigits(int n) {
+        if (n == 0)
+            throw new IllegalArgumentException("Zero digits is not allowed for a regex");
+        return "[0-9]{0," + n + "}";
+    }
 }
