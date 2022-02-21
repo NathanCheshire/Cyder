74e8e6e7a502489663badbdc7f67557144ea06c0
Nathan <nathan.vincent.2.718@gmail.com>
2022-02-20 20:31:30 -0600

    document IconButton

--- cyder/src/cyder/ui/IconButton.java
+++ cyder/src/cyder/ui/IconButton.java
@@ -11,17 +11,42 @@ import java.util.Objects;
  * Mainly used for DragLabel buttons.
  */
 public class IconButton extends JButton {
+    /**
+     * Supress default JButton constructor.
+     */
     private IconButton() {
         super();
     }
 
+    /**
+     * The icon to use for the icon button.
+     */
     private ImageIcon defaultIcon;
+
+    /**
+     * The hover icon to use for the icon button.
+     */
     private ImageIcon hoverAndFocusIcon;
 
+    /**
+     * Constructs a new icon button with the following parameters, and default mouse and focus listeners.
+     *
+     * @param tooltipText the tool tip text of the button
+     * @param defaultIcon the default icon
+     * @param hoverAndFocusIcon the hover icon
+     */
     public IconButton(String tooltipText, ImageIcon defaultIcon, ImageIcon hoverAndFocusIcon) {
         this(tooltipText, defaultIcon, hoverAndFocusIcon, null, null);
     }
 
+    /**
+     * Constructs a new icon button with the following parameters, and no focus listener.
+     *
+     * @param tooltipText the tool tip text of the button
+     * @param defaultIcon the default icon
+     * @param hoverAndFocusIcon the hover icon
+     * @param mouseListener the custom mouse listener for when a mouse enters/exits the icon button's area
+     */
     public IconButton(String tooltipText, ImageIcon defaultIcon, ImageIcon hoverAndFocusIcon, MouseListener mouseListener) {
         this(tooltipText, defaultIcon, hoverAndFocusIcon, mouseListener, new FocusAdapter() {
             @Override
@@ -36,6 +61,15 @@ public class IconButton extends JButton {
         });
     }
 
+    /**
+     * Constructs a new icon button with the following parameters.
+     *
+     * @param tooltipText the tooltip text of the button
+     * @param defaultIcon the default icon
+     * @param hoverAndFocusIcon the hover icon
+     * @param mouseListener the custom mouse listener for when a mouse enters/exits the icon button's area
+     * @param focusListener the focus listener for when the button gains/loses focus
+     */
     public IconButton(String tooltipText, ImageIcon defaultIcon, ImageIcon hoverAndFocusIcon,
                       MouseListener mouseListener, FocusListener focusListener) {
         if (StringUtil.isNull(tooltipText))
@@ -87,18 +121,38 @@ public class IconButton extends JButton {
         setVisible(true);
     }
 
+    /**
+     * Returns the default icon.
+     *
+     * @return the default icon
+     */
     public ImageIcon getDefaultIcon() {
         return defaultIcon;
     }
 
+    /**
+     * Sets the default icon.
+     *
+     * @param defaultIcon the default icon to use
+     */
     public void setDefaultIcon(ImageIcon defaultIcon) {
         this.defaultIcon = defaultIcon;
     }
 
+    /**
+     * Returns the hover and focus icon.
+     *
+     * @return the hover and focus icon
+     */
     public ImageIcon getHoverAndFocusIcon() {
         return hoverAndFocusIcon;
     }
 
+    /**
+     * Sets the hover and focus icon.
+     *
+     * @param hoverAndFocusIcon the hover and focus icon
+     */
     public void setHoverAndFocusIcon(ImageIcon hoverAndFocusIcon) {
         this.hoverAndFocusIcon = hoverAndFocusIcon;
     }
