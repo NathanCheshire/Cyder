488dd476cd38e825780286d12771c0ca1174d1e4
Nathan <nathan.vincent.2.718@gmail.com>
2022-05-13 14:26:50 -0500

    HarmonicRectangle logic

--- cyder/src/cyder/genesis/objects/HarmonicRectangle.java
+++ cyder/src/cyder/genesis/objects/HarmonicRectangle.java
@@ -2,6 +2,7 @@ package cyder.genesis.objects;
 
 import com.google.common.base.Preconditions;
 import cyder.constants.CyderColors;
+import cyder.exceptions.IllegalMethodException;
 import cyder.handlers.internal.ExceptionHandler;
 import cyder.threads.CyderThreadRunner;
 
@@ -9,15 +10,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class HarmonicRectangle extends JLabel {
-    private int staticWidth;
-    private int staticHeight;
+    private int currentWidth;
+    private int currentHeight;
+
+    private final int staticMaxWidth;
+    private final int staticMaxHeight;
+
+    private final int staticMinWidth;
+    private final int staticMinHeight;
 
     /**
      * The amount to increase or decrease the animation direction by.
      */
     private int animationInc = 1;
-    private int currentWidth;
-    private int currentHeight;
 
     private int delay = 50;
 
@@ -29,32 +34,47 @@ public class HarmonicRectangle extends JLabel {
         VERTICAL, HORIZONTAL
     }
 
-    public HarmonicRectangle(int width, int height) {
-        Preconditions.checkArgument(width > 0);
-        Preconditions.checkArgument(height > 0);
+    private DeltaDirection deltaDirection = DeltaDirection.INCREASING;
 
-        staticWidth = width;
-        staticHeight = height;
+    public enum DeltaDirection {
+        INCREASING, DECREASING
+    }
+
+    public HarmonicRectangle(int staticMinWidth, int staticMinHeight, int staticMaxWidth, int staticMaxHeight) {
+        Preconditions.checkArgument(staticMaxWidth > 0);
+        Preconditions.checkArgument(staticMaxHeight > 0);
+
+        Preconditions.checkArgument(staticMinWidth > 0);
+        Preconditions.checkArgument(staticMinHeight > 0);
+
+        Preconditions.checkArgument(staticMinWidth < staticMaxWidth);
+        Preconditions.checkArgument(staticMinHeight < staticMaxHeight);
+
+        this.staticMaxWidth = staticMaxWidth;
+        this.staticMaxHeight = staticMaxHeight;
+        this.staticMinWidth = staticMinWidth;
+        this.staticMinHeight = staticMinHeight;
     }
 
     @Override
     public int getWidth() {
-        return staticWidth;
-    }
-
-    public void setStaticWidth(int staticWidth) {
-        Preconditions.checkArgument(staticWidth > 0);
-        this.staticWidth = staticWidth;
+        return currentWidth;
     }
 
     @Override
     public int getHeight() {
-        return staticHeight;
+        return currentHeight;
     }
 
-    public void setStaticHeight(int staticHeight) {
-        Preconditions.checkArgument(staticHeight > 0);
-        this.staticHeight = staticHeight;
+    @Override
+    public void setSize(int width, int height) {
+        throw new IllegalMethodException("Illegal method; if invoking inside of class, use super modifier");
+    }
+
+    @Override
+    public void setBounds(int x, int y, int width, int height) {
+        throw new IllegalMethodException("Illegal method; if invoking inside of class," +
+                " use super modifier. If attempting to set anchor point, invoke setSize()");
     }
 
     public int getDelay() {
@@ -91,24 +111,43 @@ public class HarmonicRectangle extends JLabel {
         this.animationInc = animationInc;
     }
 
-    private boolean animate;
+    private boolean shouldAnimate;
 
     public void startAnimation() {
-        if (!animate) {
-            animate = true;
+        if (!shouldAnimate) {
+            shouldAnimate = true;
 
             // todo extract lamba runnable
             CyderThreadRunner.submit(() -> {
-                while (animate) {
+                while (shouldAnimate) {
                     try {
                         // todo add delta to thing
                         switch (harmonicDirection) {
                             case VERTICAL:
+                                switch (deltaDirection) {
+                                    case INCREASING:
+                                        if (currentHeight + animationInc < staticMaxHeight) {
+                                            currentHeight += animationInc;
+                                        } else {
+                                            currentHeight = staticMaxHeight;
+                                            deltaDirection = DeltaDirection.DECREASING;
+                                        }
 
+                                        break;
+                                    case DECREASING:
+                                        if (currentHeight - animationInc > 0) {
+                                            currentHeight -= animationInc;
+                                        } else {
+                                            currentHeight = 0;
+                                        }
+
+                                        break;
+                                    default:
+                                        throw new IllegalStateException("Invalid delta direction: " + deltaDirection);
+                                }
 
                                 break;
                             case HORIZONTAL:
-                                // todo need a way to figure out if going down or up currently
 
                                 break;
                             default:
@@ -125,7 +164,7 @@ public class HarmonicRectangle extends JLabel {
     }
 
     public void stopAnimation() {
-        animate = false;
+        shouldAnimate = false;
     }
 
     @Override
