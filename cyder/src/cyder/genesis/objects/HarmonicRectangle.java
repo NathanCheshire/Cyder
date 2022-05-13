8ef377e6d6b50c6aca7ec82abe6601343339cec9
Nathan <nathan.vincent.2.718@gmail.com>
2022-05-12 21:44:10 -0500

    HarmonicRectangle work

--- cyder/src/cyder/genesis/objects/HarmonicRectangle.java
+++ cyder/src/cyder/genesis/objects/HarmonicRectangle.java
@@ -1,6 +1,9 @@
 package cyder.genesis.objects;
 
+import com.google.common.base.Preconditions;
 import cyder.constants.CyderColors;
+import cyder.handlers.internal.ExceptionHandler;
+import cyder.threads.CyderThreadRunner;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,23 +15,26 @@ public class HarmonicRectangle extends JLabel {
     /**
      * The amount to increase or decrease the animation direction by.
      */
-    private int animateDelta;
+    private int animationInc = 1;
     private int currentWidth;
     private int currentHeight;
 
-    private int delay;
+    private int delay = 50;
 
     private Color backgroundColor = CyderColors.vanila;
 
-    private AlternationDirection alternationDirection = AlternationDirection.VERTICAL;
+    private HarmonicDirection harmonicDirection = HarmonicDirection.VERTICAL;
 
-    public enum AlternationDirection {
+    public enum HarmonicDirection {
         VERTICAL, HORIZONTAL
     }
 
     public HarmonicRectangle(int width, int height) {
-        this.staticWidth = width;
-        this.staticHeight = height;
+        Preconditions.checkArgument(width > 0);
+        Preconditions.checkArgument(height > 0);
+
+        staticWidth = width;
+        staticHeight = height;
     }
 
     @Override
@@ -37,6 +43,7 @@ public class HarmonicRectangle extends JLabel {
     }
 
     public void setStaticWidth(int staticWidth) {
+        Preconditions.checkArgument(staticWidth > 0);
         this.staticWidth = staticWidth;
     }
 
@@ -46,6 +53,7 @@ public class HarmonicRectangle extends JLabel {
     }
 
     public void setStaticHeight(int staticHeight) {
+        Preconditions.checkArgument(staticHeight > 0);
         this.staticHeight = staticHeight;
     }
 
@@ -54,6 +62,7 @@ public class HarmonicRectangle extends JLabel {
     }
 
     public void setDelay(int delay) {
+        Preconditions.checkArgument(delay > 0);
         this.delay = delay;
     }
 
@@ -65,20 +74,58 @@ public class HarmonicRectangle extends JLabel {
         this.backgroundColor = backgroundColor;
     }
 
-    public AlternationDirection getAlternationDirection() {
-        return alternationDirection;
+    public HarmonicDirection getAlternationDirection() {
+        return harmonicDirection;
     }
 
-    public void setAlternationDirection(AlternationDirection alternationDirection) {
-        this.alternationDirection = alternationDirection;
+    public void setAlternationDirection(HarmonicDirection harmonicDirection) {
+        this.harmonicDirection = harmonicDirection;
     }
 
-    public int getAnimateDelta() {
-        return animateDelta;
+    public int getAnimationInc() {
+        return animationInc;
     }
 
-    public void setAnimateDelta(int animateDelta) {
-        this.animateDelta = animateDelta;
+    public void setAnimationInc(int animationInc) {
+        Preconditions.checkArgument(animationInc > 0);
+        this.animationInc = animationInc;
+    }
+
+    private boolean animate;
+
+    public void startAnimation() {
+        if (!animate) {
+            animate = true;
+
+            // todo extract lamba runnable
+            CyderThreadRunner.submit(() -> {
+                while (animate) {
+                    try {
+                        // todo add delta to thing
+                        switch (harmonicDirection) {
+                            case VERTICAL:
+
+
+                                break;
+                            case HORIZONTAL:
+                                // todo need a way to figure out if going down or up currently
+
+                                break;
+                            default:
+                                throw new IllegalStateException("Invalid harmonic direction: " + harmonicDirection);
+                        }
+
+                        Thread.sleep(delay);
+                    } catch (Exception e) {
+                        ExceptionHandler.handle(e);
+                    }
+                }
+            }, "Harmonic Rectangle Animator");
+        }
+    }
+
+    public void stopAnimation() {
+        animate = false;
     }
 
     @Override
@@ -86,6 +133,11 @@ public class HarmonicRectangle extends JLabel {
         Graphics2D g2d = (Graphics2D) g;
 
         g2d.setColor(backgroundColor);
-        g2d.fillRect(0, 0, currentWidth, currentHeight);
+        // todo this implies the anchor point is static, will need to be updated if
+        //  animating the left or top sides are desired and not just bottom and right
+        g2d.fillRect(getX(), getY(), currentWidth, currentHeight);
+
+
+        g2d.dispose(); //todo is this a resource leak throughout Cyder where dispose calls are not used?
     }
 }
