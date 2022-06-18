978c9192dd3d71b231c434eeb8835766f14285d6
Nathan <nathan.vincent.2.718@gmail.com>
2022-06-18 17:26:59 -0500

    fix local backend bugs

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,7 @@ import os
 
 app = FastAPI()
 
+
 class AudioLengthPost(BaseModel):
     audio_path: str
 
@@ -16,16 +17,21 @@ class AudioLengthPost(BaseModel):
 def read_root():
     return {"ping_time": get_unix_gmt_time()}
 
+
 class ColorPost(BaseModel):
     image: str
+
 
 @app.post("/image/find-color")
 def post_best_color(color: ColorPost):
     return {"color": str(find_best_color(np.array(Image.open(color.image))))}
 
+
 @app.post("/audio/length/")
-def get_audio_length(audio_length: AudioLengthPost):
-    print(audio_length)
-    if (os.path.exists(audio_length.audio_path)):
+def post_audio_length(audio_length: AudioLengthPost):
+    exists = os.path.exists(audio_length.audio_path)
+
+    if exists:
         return {"length": get_audio_length(audio_length.audio_path)}
-    
+    else:
+        return {"error": "file not found"}
---
+++ backend/setup.bat
@@ -0,0 +1,4 @@
+call python -m venv venv
+call .\venv\Scripts\Activate.bat
+call pip install -r requirements.txt
+call python color.py
