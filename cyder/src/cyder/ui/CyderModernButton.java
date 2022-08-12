d8930cf11c0358af9df490937b53a8e3713ac562
Nathan <nathan.vincent.2.718@gmail.com>
2022-08-11 20:08:23 -0500

    modern button work

--- cyder/src/cyder/ui/CyderModernButton.java
+++ cyder/src/cyder/ui/CyderModernButton.java
@@ -5,6 +5,7 @@ import cyder.constants.CyderFonts;
 import cyder.handlers.internal.ExceptionHandler;
 import cyder.threads.CyderThreadRunner;
 import cyder.threads.ThreadUtil;
+import cyder.utils.StringUtil;
 
 import javax.swing.*;
 import java.awt.*;
@@ -17,7 +18,8 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * A moder button for use throughout Cyder, similar to {@link CyderCheckbox}.
  */
 public class CyderModernButton extends JLabel {
-    private String text = "Modern Button";
+    private String text;
+    private static final String DEFAULT_TEXT = "Modern Button";
 
     private Font font = CyderFonts.DEFAULT_FONT_SMALL;
 
@@ -32,7 +34,11 @@ public class CyderModernButton extends JLabel {
     private boolean roundedCorners = true;
     private boolean threadsKilled = false;
     private boolean disabled = false;
+    private boolean isFlashing;
 
+    /**
+     * The radius to use when {@link #roundedCorners} is enabled.
+     */
     private static final int CORNER_RADIUS = 20;
     private int borderRadius = 3;
 
@@ -41,32 +47,131 @@ public class CyderModernButton extends JLabel {
 
     private JLabel innerTextLabel;
 
-    private final AtomicBoolean mouseIn = new AtomicBoolean();
+    private final AtomicBoolean mouseInside = new AtomicBoolean();
 
     public CyderModernButton() {
+        this(DEFAULT_TEXT);
+    }
+
+    public CyderModernButton(String text) {
+        this.text = text;
+
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
-                mouseIn.set(true);
+                mouseInside.set(true);
                 repaint();
             }
 
             @Override
             public void mouseExited(MouseEvent e) {
-                mouseIn.set(false);
+                mouseInside.set(false);
                 repaint();
             }
         });
 
+        installInnerTextLabel();
+    }
+
+    /**
+     * Sets up and adds the inner text label to this component.
+     */
+    private void installInnerTextLabel() {
         innerTextLabel = new JLabel();
-        innerTextLabel.setHorizontalAlignment(JLabel.CENTER);
-        // todo enum for config of positioning, top left, bottom right, etc.
-        innerTextLabel.setVerticalAlignment(JLabel.CENTER);
+
+        refreshInnerTextLabel();
+
+        add(innerTextLabel);
+    }
+
+    /**
+     * Refreshes the text, bounds, font, foreground, and alignment of the inner text label.
+     */
+    private void refreshInnerTextLabel() {
+        innerTextLabel.setText(text);
         innerTextLabel.setBounds(0, 0, width, height);
         innerTextLabel.setFont(font);
         innerTextLabel.setForeground(foregroundColor);
-        innerTextLabel.setText(text);
-        add(innerTextLabel);
+
+        refreshTextAlignment();
+
+        innerTextLabel.repaint();
+    }
+
+    /**
+     * The alignment of the text of the modern button.
+     */
+    public enum TextAlignment {
+        TOP_LEFT, TOP, TOP_RIGHT,
+        LEFT, CENTER, RIGHT,
+        BOTTOM_LEFT, BOTTOM, BOTTOM_RIGHT
+    }
+
+    /**
+     * The text alignment of the button text.
+     */
+    private TextAlignment textAlignment = TextAlignment.CENTER;
+
+    /**
+     * Returns the text alignment of the button text.
+     *
+     * @return the text alignment of the button text
+     */
+    public TextAlignment getTextAlignment() {
+        return textAlignment;
+    }
+
+    /**
+     * Sets the text alignment of the button text.
+     *
+     * @param textAlignment the text alignment of the button text
+     */
+    public void setTextAlignment(TextAlignment textAlignment) {
+        this.textAlignment = textAlignment;
+    }
+
+    /**
+     * Refreshes the text alignment of the inner text label.
+     */
+    private void refreshTextAlignment() {
+        switch (textAlignment) {
+            case TOP_LEFT -> {
+                innerTextLabel.setHorizontalAlignment(JLabel.LEFT);
+                innerTextLabel.setVerticalAlignment(JLabel.TOP);
+            }
+            case TOP -> {
+                innerTextLabel.setHorizontalAlignment(JLabel.CENTER);
+                innerTextLabel.setVerticalAlignment(JLabel.TOP);
+            }
+            case TOP_RIGHT -> {
+                innerTextLabel.setHorizontalAlignment(JLabel.RIGHT);
+                innerTextLabel.setVerticalAlignment(JLabel.TOP);
+            }
+            case LEFT -> {
+                innerTextLabel.setHorizontalAlignment(JLabel.LEFT);
+                innerTextLabel.setVerticalAlignment(JLabel.CENTER);
+            }
+            case CENTER -> {
+                innerTextLabel.setHorizontalAlignment(JLabel.CENTER);
+                innerTextLabel.setVerticalAlignment(JLabel.CENTER);
+            }
+            case RIGHT -> {
+                innerTextLabel.setHorizontalAlignment(JLabel.RIGHT);
+                innerTextLabel.setVerticalAlignment(JLabel.CENTER);
+            }
+            case BOTTOM_LEFT -> {
+                innerTextLabel.setHorizontalAlignment(JLabel.LEFT);
+                innerTextLabel.setVerticalAlignment(JLabel.BOTTOM);
+            }
+            case BOTTOM -> {
+                innerTextLabel.setHorizontalAlignment(JLabel.CENTER);
+                innerTextLabel.setVerticalAlignment(JLabel.BOTTOM);
+            }
+            case BOTTOM_RIGHT -> {
+                innerTextLabel.setHorizontalAlignment(JLabel.RIGHT);
+                innerTextLabel.setVerticalAlignment(JLabel.BOTTOM);
+            }
+        }
     }
 
     /**
@@ -81,16 +186,22 @@ public class CyderModernButton extends JLabel {
         qualityHints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         graphics2D.setRenderingHints(qualityHints);
 
+        graphics2D.setPaint(borderColor);
+        graphics2D.setStroke(new BasicStroke(1.0f));
+
         if (roundedCorners) {
-            graphics2D.setPaint(borderColor);
-            graphics2D.setStroke(new BasicStroke(1.0f));
             graphics2D.fill(new RoundRectangle2D.Double(0, 0, width, height, CORNER_RADIUS, CORNER_RADIUS));
-            graphics2D.setPaint(mouseIn.get() ? hoverColor : backgroundColor);
+            graphics2D.setPaint(mouseInside.get() ? hoverColor : backgroundColor);
             graphics2D.fill(new RoundRectangle2D.Double(borderRadius, borderRadius,
                     width - 2 * borderRadius, height - 2 * borderRadius, CORNER_RADIUS, CORNER_RADIUS));
         } else {
-            // todo
+            graphics2D.fillRect(0, 0, width, height);
+            graphics2D.setPaint(mouseInside.get() ? hoverColor : backgroundColor);
+            graphics2D.fillRect(borderRadius, borderRadius,
+                    width - 2 * borderRadius, height - 2 * borderRadius);
         }
+
+
     }
 
     /**
@@ -127,6 +238,65 @@ public class CyderModernButton extends JLabel {
         }
 
         repaint();
+    }
+
+    /**
+     * Expands the button's width or height as necessary to fit all of the button text on the button.
+     */
+    public void expandToMinSize() {
+        expandToMinSize(false);
+    }
+
+    /**
+     * Expands the button's width or height as necessary to fit all of the button text on the button.
+     * The center of the button is kept in the same place if maintainRelativeCenter is passed as true.
+     *
+     * @param maintainRelativeCenter whether to maintain the center of the button
+     */
+    public void expandToMinSize(boolean maintainRelativeCenter) {
+        int necessaryWidth = StringUtil.getMinWidth(text, font);
+        int necessaryHeight = StringUtil.getMinHeight(text, font);
+
+        int newWidth = Math.max(width, necessaryWidth);
+        int newHeight = Math.max(height, necessaryHeight);
+
+        if (newWidth != width || newHeight != height) {
+            if (maintainRelativeCenter) {
+                Point center = getCenter();
+                setBounds((int) (center.getX() - newWidth / 2), (int) (center.getY() - newHeight / 2),
+                        newWidth, newHeight);
+            } else {
+                setSize(newWidth, newHeight);
+            }
+        }
+    }
+
+    /**
+     * Sets the center of the button on the parent component to the specified point.
+     *
+     * @param point the point to set as the button's center
+     */
+    public void setCenter(Point point) {
+        setCenter(point.x, point.y);
+    }
+
+    /**
+     * Sets the center of the button on the parent component to the specified point.
+     *
+     * @param x the x value of the point
+     * @param y the y value of the point
+     */
+    public void setCenter(int x, int y) {
+        setLocation(x - this.getWidth() / 2, y - this.getHeight() / 2);
+    }
+
+    /**
+     * Returns the center point of the button on the parent component.
+     *
+     * @return the center point of the button on the parent component
+     */
+    public Point getCenter() {
+        return new Point(this.getX() + this.getWidth() / 2, this.getY() + this.getHeight() / 2);
     }
 
     /**
