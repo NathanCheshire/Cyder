e6c8b6760a809b4a2efbd2957d162affd62ced32
ncheshire <ncheshire@camgian.com>
2022-08-12 12:43:48 -0500

    fix new line detector

--- actions/new_line_detector.py
+++ actions/new_line_detector.py
@@ -1,36 +1,22 @@
-
-
 import sys
 from stat_generator import find_files
 
 
-def find_new_lines():
+def find_unnecessary_new_lines():
     """
     Finds unnecessary new lines in all .java files recursively found starting from this directory.
     Unnecessary new lines are defined by a length of two or more for Cyder.
     """
+    failed = False
+    java_files = find_files(starting_dir=".", extensions=['.java'], recursive=True)
 
-    total_unnecessary_new_lines = 0
+    for file in java_files:
+        current_running_new_lines = 0
 
-    for file in find_files(starting_dir=".", extensions=['.java'], recursive=True):
-        def print_stats(current_file: str, num_lines: int, starting_line: str, ending_line: str,
-                        starting_line_num: int, ending_line_num: int):
-            """
-            Prints the statistics found for the current file.
-            """
-
-            print("File:", current_file)
-            print("Found", num_lines, "new lines between:")
-            print(starting_line_num, ":", starting_line)
-            print("and")
-            print(ending_line_num + 1, ":", ending_line)
-            print("-------------------------------")
-
-        local_unnecessary_new_lines = 0
         anchored = False
-        last_anchor_line = None
-        last_anchor_line_number = 0
         originally_anchored = False
+        last_anchor = None
+        last_anchor_line_number = 0
 
         file_lines = open(file, 'r').readlines()
 
@@ -41,25 +27,38 @@ def find_new_lines():
             if empty and not originally_anchored:
                 continue
             elif not empty and not anchored:
-                if local_unnecessary_new_lines > 1:
-                    print_stats(file, local_unnecessary_new_lines, last_anchor_line, line,
-                                last_anchor_line_number, line_index + 1)
+                if current_running_new_lines > 1:
+                    print_stats(file, current_running_new_lines, last_anchor_line_number,
+                                last_anchor, line_index + 1, line)
+                    failed = True
 
                 originally_anchored = True
                 anchored = True
-                last_anchor_line = line
-                local_unnecessary_new_lines = 0
+                last_anchor = line
+                current_running_new_lines = 0
             elif empty:
-                if local_unnecessary_new_lines == 0:
+                if current_running_new_lines == 0:
                     last_anchor_line_number = line_index
 
                 anchored = False
-                local_unnecessary_new_lines = local_unnecessary_new_lines + 1
+                current_running_new_lines = current_running_new_lines + 1
 
-        total_unnecessary_new_lines += local_unnecessary_new_lines
-                
-    sys.exit(total_unnecessary_new_lines)
+    sys.exit(1 if failed else 0)
+
+
+def print_stats(file: str, num_unnecessary_new_lines: int, starting_line_num: int,
+                starting_line: str, ending_list_num: int, ending_line: str):
+    """
+    Prints the statistics found for the current file.
+    """
+
+    print("File:", file)
+    print("Found", num_unnecessary_new_lines, "new lines between:")
+    print(starting_line_num, ":", starting_line)
+    print("and")
+    print(ending_list_num, ":", ending_line)
+    print("-------------------------------")
 
 
 if __name__ == '__main__':
-    find_new_lines()
+    find_unnecessary_new_lines()
