53a9282653fc459d57c6b541a20c5d0fc23b6070
Nathan <nathan.vincent.2.718@gmail.com>
2022-08-25 20:25:12 -0500

    add periods to CyderColors

--- cyder/src/cyder/constants/CyderColors.java
+++ cyder/src/cyder/constants/CyderColors.java
@@ -13,62 +13,62 @@ import java.awt.*;
 @SuppressWarnings("unused")
 public final class CyderColors {
     /**
-     * A common color used for selected text such as in CyderFields
+     * A common color used for selected text such as in CyderFields.
      */
     public static final Color selectionColor = new Color(204, 153, 0);
 
     /**
-     * A default green color, never to be changed
+     * A default green color, never to be changed.
      */
     public static final Color regularGreen = new Color(60, 167, 92);
 
     /**
-     * A default blue color, never to be changed
+     * A default blue color, never to be changed.
      */
     public static final Color regularBlue = new Color(38, 168, 255);
 
     /**
-     * A default red color, never to be changed
+     * A default red color, never to be changed.
      */
     public static final Color regularRed = new Color(223, 85, 83);
 
     /**
-     * A default pink color, never to be changed
+     * A default pink color, never to be changed.
      */
     public static final Color regularPink = new Color(236, 64, 122);
 
     /**
-     * An alternative red color
+     * An alternative red color.
      */
     public static final Color snapchatRed = new Color(242, 59, 87);
 
     /**
-     * A default orange color, never to be changed
+     * A default orange color, never to be changed.
      */
     public static final Color regularOrange = new Color(255, 140, 0);
 
     /**
-     * The default text color to use for text when dark mode is active
+     * The default text color to use for text when dark mode is active.
      */
     public static final Color defaultDarkModeTextColor = new Color(240, 240, 240);
 
     /**
-     * The default text color to use for text when dark mode is active
+     * The default text color to use for text when dark mode is active.
      */
     public static final Color defaultLightModeTextColor = new Color(16, 16, 16);
 
     /**
-     * An empty color
+     * An empty color.
      */
     public static final Color empty = new Color(0, 0, 0, 0);
 
     /**
-     * A default navy color that is used extensively throughout Cyder
+     * A default navy color that is used extensively throughout Cyder.
      */
     public static final Color navy = new Color(26, 32, 51);
 
     /**
-     * The color used for notification borders
+     * The color used for notification borders.
      */
     public static final Color notificationBorderColor = new Color(26, 32, 51);
 
@@ -78,27 +78,27 @@ public final class CyderColors {
     public static final Color regularPurple = new Color(85, 85, 255);
 
     /**
-     * The foreground color used for notifications
+     * The foreground color used for notifications.
      */
     public static final Color notificationForegroundColor = new Color(85, 85, 255);
 
     /**
-     * The background used for notifications
+     * The background used for notifications.
      */
     public static final Color notificationBackgroundColor = new Color(0, 0, 0);
 
     /**
-     * The default button color
+     * The default button color.
      */
     public static Color buttonColor = new Color(223, 85, 83);
 
     /**
-     * The default taskbar border color for components that are always present in the taskbar
+     * The default taskbar border color for components that are always present in the taskbar.
      */
     public static final Color taskbarDefaultColor = new Color(137, 84, 160);
 
     /**
-     * A common white color used throughout Cyder
+     * A common white color used throughout Cyder.
      */
     public static final Color vanilla = new Color(252, 252, 252);
 
@@ -127,19 +127,19 @@ public final class CyderColors {
     );
 
     /**
-     * Instantiation of the CyderColors class is not allowed
+     * Instantiation of the CyderColors class is not allowed.
      */
     private CyderColors() {
         throw new IllegalMethodException(CyderStrings.ATTEMPTED_INSTANTIATION);
     }
 
     /**
-     * The color to use for frame borders and other ui components throughout Cyder
+     * The color to use for frame borders and other ui components throughout Cyder.
      */
     private static Color guiThemeColor = navy;
 
     /**
-     * Sets the current window border color/UI color
+     * Sets the current window border color/UI color.
      *
      * @param c the color to use for frame borders and other ui components
      */
