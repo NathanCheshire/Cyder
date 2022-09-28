4c89200466c925bc6b5b683e968c879799397acb
Nathan <nathan.vincent.2.718@gmail.com>
2022-09-27 21:17:32 -0500

    improve click widget readability

--- cyder/src/cyder/widgets/ClickWidget.java
+++ cyder/src/cyder/widgets/ClickWidget.java
@@ -1,6 +1,7 @@
 package cyder.widgets;
 
 import cyder.annotations.CyderAuthor;
+import cyder.annotations.ForReadability;
 import cyder.annotations.Vanilla;
 import cyder.annotations.Widget;
 import cyder.constants.CyderColors;
@@ -10,12 +11,16 @@ import cyder.exceptions.IllegalMethodException;
 import cyder.handlers.internal.ExceptionHandler;
 import cyder.ui.frame.CyderFrame;
 import cyder.utils.NumberUtil;
+import cyder.utils.UiUtil;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+/**
+ * A widget for extreme boredom.
+ */
 @Vanilla
 @CyderAuthor
 public final class ClickWidget {
@@ -26,53 +31,96 @@ public final class ClickWidget {
         throw new IllegalMethodException(CyderStrings.ATTEMPTED_INSTANTIATION);
     }
 
+    /**
+     * The frame title and label text.
+     */
+    private static final String CLICK_ME = "Click Me";
+
+    /**
+     * The frame width.
+     */
+    private static final int FRAME_WIDTH = 220;
+
+    /**
+     * The frame height.
+     */
+    private static final int FRAME_HEIGHT = 100;
+
+    /**
+     * The widget frame.
+     */
+    private static CyderFrame clickMeFrame;
+
+    /**
+     * The offset between the monitor bounds and the possible locations to place the frame at.
+     */
+    private static final int monitorMinOffset = 200;
+
+    /**
+     * The font for the click me label.
+     */
+    private static final Font clickMeLabelFont = new Font(CyderFonts.SEGOE_UI_BLACK, Font.BOLD, 22);
+
     @Widget(triggers = "click me", description = "A troll widget that pops open a new window every time it is clicked")
     public static void showGui() {
         try {
-            CyderFrame clickMeFrame = new CyderFrame(220, 100) {
+            UiUtil.closeIfOpen(clickMeFrame);
+
+            clickMeFrame = new CyderFrame(FRAME_WIDTH, FRAME_HEIGHT) {
                 @Override
                 public void dispose() {
                     dispose(true);
                 }
             };
-            clickMeFrame.setTitle("Click Me");
+            clickMeFrame.setTitle(CLICK_ME);
             clickMeFrame.setFrameType(CyderFrame.FrameType.POPUP);
             clickMeFrame.setBackground(CyderColors.vanilla);
 
-            JLabel dismiss = new JLabel("Click Me!");
-            dismiss.setHorizontalAlignment(JLabel.CENTER);
-            dismiss.setVerticalAlignment(JLabel.CENTER);
-            dismiss.setForeground(CyderColors.navy);
-            dismiss.setFont(CyderFonts.SEGOE_30.deriveFont(22f));
-            dismiss.setBounds(30, 40, 150, 40);
-            dismiss.addMouseListener(new MouseAdapter() {
+            JLabel clickMeLabel = new JLabel(CLICK_ME);
+            clickMeLabel.setHorizontalAlignment(JLabel.CENTER);
+            clickMeLabel.setVerticalAlignment(JLabel.CENTER);
+            clickMeLabel.setForeground(CyderColors.navy);
+            clickMeLabel.setFont(clickMeLabelFont);
+            clickMeLabel.setBounds(30, 40, 150, 40);
+            clickMeLabel.addMouseListener(new MouseAdapter() {
                 @Override
                 public void mouseReleased(MouseEvent e) {
-                    clickMeFrame.dispose(true);
-                    showGui();
+                    changeFramePosition();
                 }
 
                 @Override
                 public void mouseEntered(MouseEvent e) {
-                    dismiss.setForeground(CyderColors.regularRed);
+                    clickMeLabel.setForeground(CyderColors.regularRed);
                 }
 
                 @Override
                 public void mouseExited(MouseEvent e) {
-                    dismiss.setForeground(CyderColors.navy);
+                    clickMeLabel.setForeground(CyderColors.navy);
                 }
             });
 
-            clickMeFrame.getContentPane().add(dismiss);
-            clickMeFrame.setVisible(true);
+            clickMeFrame.getContentPane().add(clickMeLabel);
 
-            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-            GraphicsDevice defaultScreen = ge.getDefaultScreenDevice();
-            Rectangle rect = defaultScreen.getDefaultConfiguration().getBounds();
-            clickMeFrame.setLocation(NumberUtil.randInt((int) (rect.getMaxX() - 200)),
-                    NumberUtil.randInt((int) rect.getMaxY() - 200));
+            clickMeFrame.finalizeAndShow();
         } catch (Exception e) {
             ExceptionHandler.handle(e);
         }
+    }
+
+    /**
+     * The logic for when the click me label is pressed.
+     */
+    @ForReadability
+    private static void changeFramePosition() {
+        Rectangle bounds = clickMeFrame.getMonitorBounds();
+        int minX = (int) bounds.getMinX();
+        int maxX = (int) (minX + bounds.getHeight());
+        int minY = (int) bounds.getMinY();
+        int maxY = (int) (minY + bounds.getHeight());
+
+        int randomX = NumberUtil.randInt(minX + monitorMinOffset, maxX - monitorMinOffset);
+        int randomY = NumberUtil.randInt(minY + monitorMinOffset, maxY - monitorMinOffset);
+
+        clickMeFrame.setLocation(randomX, randomY);
     }
 }
