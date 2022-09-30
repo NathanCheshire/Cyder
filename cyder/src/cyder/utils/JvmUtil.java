e110a4372d41feb946c0ae4fc792ba7bb9d44eb8
Nathan <nathan.vincent.2.718@gmail.com>
2022-09-29 21:09:29 -0500

    jvm util clean up

--- cyder/src/cyder/utils/JvmUtil.java
+++ cyder/src/cyder/utils/JvmUtil.java
@@ -23,15 +23,30 @@ public final class JvmUtil {
     private static final String IN_DEBUG_MODE_KEY_PHRASE = "-agentlib:jdwp";
 
     /**
+     * Whether the current jvm session is in debug mode meaning threads could be externally suspended.
+     */
+    private static final boolean JVM_LAUNCHED_IN_DEBUG_MODE = ManagementFactory.getRuntimeMXBean()
+            .getInputArguments().toString().contains(IN_DEBUG_MODE_KEY_PHRASE);
+
+    /**
      * Returns whether the current jvm session is in debug mode meaning
-     * thread could be externally suspended.
+     * threads could be externally suspended.
      *
      * @return whether the current jvm session is in debug mode meaning
-     * thread could be externally suspended
+     * threads could be externally suspended
      */
     public static boolean currentInstanceLaunchedWithDebug() {
-        //System.out.println(ManagementFactory.getClassLoadingMXBean().getTotalLoadedClassCount());
-        return ManagementFactory.getRuntimeMXBean()
-                .getInputArguments().toString().contains(IN_DEBUG_MODE_KEY_PHRASE);
+        return JVM_LAUNCHED_IN_DEBUG_MODE;
+    }
+
+    /**
+     * Returns the total number of classes that have been loaded
+     * since the Java virtual machine has started execution.
+     *
+     * @return the total number of classes that have been loaded
+     * since the Java virtual machine has started execution
+     */
+    public static long getCurrentTotalLoadedClassCount() {
+        return ManagementFactory.getClassLoadingMXBean().getTotalLoadedClassCount();
     }
 }
