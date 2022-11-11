0a9c4f9b5a27abef6f46adacb2733a5f8cdecd92
Nathan Cheshire <60986919+NathanCheshire@users.noreply.github.com>
2022-11-11 11:06:23 -0600

    add javadoc for exceptions package, add wrapping constructors for exceptions, add christmas linus background

--- cyder/src/cyder/exceptions/IllegalMethodException.java
+++ cyder/src/cyder/exceptions/IllegalMethodException.java
@@ -4,7 +4,17 @@ package cyder.exceptions;
  * An exception thrown when an illegal method is invoked or triggered.
  */
 public class IllegalMethodException extends IllegalArgumentException {
+    /**
+     * Constructs a new IllegalMethod exception using the provided error message.
+     */
     public IllegalMethodException(String errorMessage) {
         super(errorMessage);
+    }
+
+    /**
+     * Constructs a new IllegalMethod exception from the provided exception.
+     */
+    public IllegalMethodException(Exception e) {
+        super(e);
     }
 }
