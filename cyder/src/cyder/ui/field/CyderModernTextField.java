3a6394bcb161100d1f52ea5ce11e4ed2864f5602
Nathan <nathan.vincent.2.718@gmail.com>
2022-11-17 21:37:37 -0600

    modern text field work

--- cyder/src/cyder/ui/field/CyderModernTextField.java
+++ cyder/src/cyder/ui/field/CyderModernTextField.java
@@ -1,5 +1,7 @@
 package cyder.ui.field;
 
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Range;
 import cyder.annotations.CyderTest;
 import cyder.annotations.ForReadability;
 import cyder.constants.CyderColors;
@@ -25,15 +27,92 @@ import java.util.concurrent.atomic.AtomicInteger;
  * A modern looking text field.
  */
 public class CyderModernTextField extends JTextField {
-    private Color fieldForeground = CyderColors.navy;
-    private Font fieldFont = CyderFonts.DEFAULT_FONT_SMALL;
-    private Color fieldBackground = CyderColors.vanilla;
+    /**
+     * The range percents must fall into.
+     */
+    private static final Range<Float> percentageRange = Range.closed(0.0f, 100.0f);
 
-    private static final int defaultBorderThickness = 4;
-    private static final Color defaultBorderColor = CyderColors.navy;
+    /**
+     * The name of the ripple animation incrementer thread.
+     */
+    private static final String rippleAnimationIncrementerThreadName =
+            "CyderModernTextField Ripple Animation Incrementer";
 
-    private int borderThickness = defaultBorderThickness;
-    private Color borderColor = defaultBorderColor;
+    /**
+     * The name of the ripple animation decrementer thread.
+     */
+    private static final String rippleAnimationDecrementerThreadName =
+            "CyderModernTextField Ripple Animation Decrementer";
+
+    /**
+     * The range of valid timeouts for the ripple animation.
+     */
+    private static final Range<Integer> rippleAnimationTimeoutRange = Range.closed(1, 1000);
+
+    /**
+     * The inset values for the top, left, and right default border.
+     */
+    private static final int topLeftRightBorderInsets = 0;
+
+    /**
+     * The default ripple label thickness.
+     */
+    private static final int defaultRippleLabelThickness = 4;
+
+    /**
+     * The current ripple label thickness.
+     */
+    private int rippleLabelThickness = defaultRippleLabelThickness;
+
+    /**
+     * Whether the ripple animation should be performed
+     */
+    private final AtomicBoolean shouldPerformFocusRipple = new AtomicBoolean(true);
+
+    /**
+     * Whether this component has focus.
+     */
+    private final AtomicBoolean focused = new AtomicBoolean(false);
+
+    /**
+     * The current width of the ripple label.
+     */
+    private final AtomicInteger rippleLabelWidth = new AtomicInteger(0);
+
+    /**
+     * The ripple animation color.
+     */
+    private Color rippleColor = CyderColors.regularRed;
+
+    /**
+     * The amount to increment or decrement the ripple animation by each frame.
+     */
+    private int rippleAnimationDelta = 5;
+
+    /**
+     * The delay between ripple animation frames in ms.
+     */
+    private int rippleAnimationTimeout = 2;
+
+    /**
+     * The minimum percentage of the width the ripple label can be.
+     */
+    private float minRippleLabelLengthPercentage = percentageRange.lowerEndpoint();
+
+    /**
+     * The maximum percentage of the width the ripple label can be.
+     */
+    private float maxRippleLabelWidthPercentage = percentageRange.upperEndpoint();
+
+    /**
+     * Whether the ripple decrement animation is currently underway.
+     */
+    private final AtomicBoolean inRippleDecrementAnimation = new AtomicBoolean(false);
+
+    /**
+     * Whether the ripple increment animation is currently underway.
+     */
+    private final AtomicBoolean inRippleIncrementAnimation = new AtomicBoolean(false);
 
     /**
      * Constructs a new modern text field.
@@ -52,16 +131,16 @@ public class CyderModernTextField extends JTextField {
 
         addRippleFocusListener();
 
-        setBackground(fieldBackground);
+        setBackground(CyderColors.vanilla);
         setSelectionColor(CyderColors.selectionColor);
-        setFont(fieldFont);
-        setForeground(fieldForeground);
-        setCaretColor(fieldForeground);
-        setCaret(new CyderCaret(fieldForeground));
+        setFont(CyderFonts.DEFAULT_FONT_SMALL);
 
-        // todo this bottom value needs to always reflect the thickness set of the border currently
-        setBorder(BorderFactory.createEmptyBorder(0, 0, borderThickness, 0));
-        CyderModernTextField.this.add(borderLabel);
+        setForeground(CyderColors.navy);
+        setCaretColor(CyderColors.navy);
+        setCaret(new CyderCaret(CyderColors.navy));
+
+        refreshBorder();
+        CyderModernTextField.this.add(rippleLabel);
 
         setOpaque(true);
 
@@ -92,9 +171,64 @@ public class CyderModernTextField extends JTextField {
         throw new IllegalMethodException(CyderStrings.ILLEGAL_CONSTRUCTOR);
     }
 
-    // todo accessor mutator
-    private final AtomicBoolean shouldPerformFocusRipple = new AtomicBoolean(true);
-    private final AtomicBoolean focused = new AtomicBoolean(false);
+    /**
+     * Returns whether the ripple animation should be performed on focus events.
+     *
+     * @return whether the ripple animation should be performed on focus events
+     */
+    public boolean getShouldPerformFocusRipple() {
+        return shouldPerformFocusRipple.get();
+    }
+
+    /**
+     * Sets whether the ripple animation should be performed on focus events.
+     *
+     * @param shouldPerformFocusRipple whether the ripple animation should be performed on focus events
+     */
+    public void setShouldPerformFocusRipple(boolean shouldPerformFocusRipple) {
+        this.shouldPerformFocusRipple.set(shouldPerformFocusRipple);
+    }
+
+    /**
+     * Returns the ripple label thickness.
+     *
+     * @return the ripple label thickness
+     */
+    public int getRippleLabelThickness() {
+        return rippleLabelThickness;
+    }
+
+    /**
+     * Sets the ripple label thickness.
+     *
+     * @param rippleLabelThickness the ripple label thickness
+     */
+    public void setRippleLabelThickness(int rippleLabelThickness) {
+        this.rippleLabelThickness = rippleLabelThickness;
+
+        refreshBorder();
+        refreshRippleLabelBounds();
+    }
+
+    /**
+     * Returns the ripple color.
+     *
+     * @return the ripple color
+     */
+    public Color getRippleColor() {
+        return rippleColor;
+    }
+
+    /**
+     * Sets the ripple color.
+     *
+     * @param rippleColor the ripple color
+     */
+    public void setRippleColor(Color rippleColor) {
+        Preconditions.checkNotNull(rippleColor);
+
+        this.rippleColor = rippleColor;
+    }
 
     /**
      * Adds the ripple focus listener to this text field.
@@ -105,85 +239,191 @@ public class CyderModernTextField extends JTextField {
             @Override
             public void focusGained(FocusEvent e) {
                 focused.set(true);
-                if (!shouldPerformFocusRipple.get()) return;
-                startRippleAnimation();
+                refreshRippleLabelBounds();
+
+                if (!shouldPerformFocusRipple.get()) {
+                    rippleLabelWidth.set((int) (getWidth() * maxRippleLabelWidthPercentage));
+                    rippleLabel.setSize(rippleLabelWidth.get(), rippleLabelThickness);
+                    rippleLabel.repaint();
+                    return;
+                }
+
+                startRippleIncrementAnimation();
             }
 
             @Override
             public void focusLost(FocusEvent e) {
                 focused.set(false);
-                if (!shouldPerformFocusRipple.get()) return;
-                recedeRippleAnimation();
+                refreshRippleLabelBounds();
+
+                if (!shouldPerformFocusRipple.get()) {
+                    rippleLabelWidth.set((int) (getWidth() * minRippleLabelLengthPercentage));
+                    rippleLabel.setSize(rippleLabelWidth.get(), rippleLabelThickness);
+                    rippleLabel.repaint();
+                    return;
+                }
+
+                startRippleDecrementAnimation();
             }
         });
     }
 
-    // TODO min and max lengths for border
-
-    private final AtomicInteger borderLabelWidth = new AtomicInteger(0);
-
-    private final JLabel borderLabel = new JLabel() {
+    /**
+     * The label for the ripple animation.
+     */
+    private final JLabel rippleLabel = new JLabel() {
         @Override
         public void paint(Graphics g) {
-            g.setColor(CyderColors.regularRed);
-            g.fillRect(0, 0, borderLabelWidth.get(), borderThickness);
+            g.setColor(rippleColor);
+            g.fillRect(0, 0, rippleLabelWidth.get(), rippleLabelThickness);
         }
     };
 
     /**
-     * The amount to increment or decrement the ripple animation by each frame.
+     * Returns the increment/decrement for the ripple animation.
+     *
+     * @return the increment/decrement for the ripple animation
      */
-    private int rippleAnimationDelta = 5;
+    public int getRippleAnimationDelta() {
+        return rippleAnimationDelta;
+    }
 
     /**
-     * The delay between ripple animation frames in ms.
+     * Sets the increment/decrement for the ripple animation.
+     *
+     * @param rippleAnimationDelta the increment/decrement for the ripple animation
      */
-    private int rippleAnimationTimeout = 2;
+    public void setRippleAnimationDelta(int rippleAnimationDelta) {
+        Preconditions.checkArgument(rippleAnimationDelta > 0);
+
+        this.rippleAnimationDelta = rippleAnimationDelta;
+    }
 
     /**
-     * The name of the ripple animation incrementer thread.
+     * Returns the timeout between ripple animation frames in ms.
+     *
+     * @return the timeout between ripple animation frames in ms
      */
-    private static final String rippleAnimationIncrementerThreadName =
-            "CyderModernTextField Ripple Animation Incrementer";
+    public int getRippleAnimationTimeout() {
+        return rippleAnimationTimeout;
+    }
 
+    /**
+     * Sets the timeout between ripple animation frames in ms.
+     *
+     * @param rippleAnimationTimeout the timeout between ripple animation frames in ms
+     */
+    public void setRippleAnimationTimeout(int rippleAnimationTimeout) {
+        Preconditions.checkArgument(rippleAnimationTimeoutRange.contains(rippleAnimationTimeout));
+
+        this.rippleAnimationTimeout = rippleAnimationTimeout;
+    }
+
+    /**
+     * Returns whether the ripple increment animation is currently underway.
+     *
+     * @return the ripple increment animation is currently underway
+     */
+    public boolean inRippleIncrementAnimation() {
+        return inRippleIncrementAnimation.get();
+    }
+
+    /**
+     * Starts the increment ripple animation.
+     */
     @ForReadability
-    private void startRippleAnimation() {
-        refreshRippleLabelBounds();
+    private void startRippleIncrementAnimation() {
+        Preconditions.checkState(focused.get());
+
+        inRippleIncrementAnimation.set(true);
 
         CyderThreadRunner.submit(() -> {
-            while (borderLabelWidth.get() < getWidth() && focused.get()) {
-                borderLabelWidth.getAndAdd(rippleAnimationDelta);
-                borderLabel.setSize(borderLabelWidth.get(), borderThickness);
-                borderLabel.repaint();
+            while (rippleLabelWidth.get() < getWidth() && focused.get() && shouldPerformFocusRipple.get()) {
+                rippleLabelWidth.getAndAdd(rippleAnimationDelta);
+                rippleLabel.setSize(rippleLabelWidth.get(), rippleLabelThickness);
+                rippleLabel.repaint();
                 ThreadUtil.sleep(rippleAnimationTimeout);
             }
 
-            borderLabelWidth.set(getWidth());
-            borderLabel.setSize(borderLabelWidth.get(), borderThickness);
-            borderLabel.repaint();
+            rippleLabelWidth.set((int) (getWidth() * maxRippleLabelWidthPercentage));
+            rippleLabel.setSize(rippleLabelWidth.get(), rippleLabelThickness);
+            rippleLabel.repaint();
+
+            inRippleIncrementAnimation.set(false);
         }, rippleAnimationIncrementerThreadName);
     }
 
     /**
-     * The name of the ripple animation decrementer thread.
+     * Returns the minimum percentage of the width the ripple label can be.
+     *
+     * @return the minimum percentage of the width the ripple label can be
      */
-    private static final String rippleAnimationDecrementerThreadName =
-            "CyderModernTextField Ripple Animation Decrementer";
+    public float getMinRippleLabelLengthPercentage() {
+        return minRippleLabelLengthPercentage;
+    }
 
-    private void recedeRippleAnimation() {
-        refreshRippleLabelBounds();
+    /**
+     * Sets the minimum percentage of the width the ripple label can be.
+     *
+     * @param minRippleLabelLengthPercentage the minimum percentage of the width the ripple label can be
+     */
+    public void setMinRippleLabelLengthPercentage(float minRippleLabelLengthPercentage) {
+        Preconditions.checkArgument(percentageRange.contains(minRippleLabelLengthPercentage));
+
+        this.minRippleLabelLengthPercentage = minRippleLabelLengthPercentage;
+    }
+
+    /**
+     * Returns the maximum percentage of the width the ripple label can be.
+     *
+     * @return the maximum percentage of the width the ripple label can be
+     */
+    public float getMaxRippleLabelWidthPercentage() {
+        return maxRippleLabelWidthPercentage;
+    }
+
+    /**
+     * Sets the maximum percentage of the width the ripple label can be.
+     *
+     * @param maxRippleLabelWidthPercentage the maximum percentage of the width the ripple label can be
+     */
+    public void setMaxRippleLabelWidthPercentage(float maxRippleLabelWidthPercentage) {
+        Preconditions.checkArgument(percentageRange.contains(maxRippleLabelWidthPercentage));
+
+        this.maxRippleLabelWidthPercentage = maxRippleLabelWidthPercentage;
+    }
+
+    /**
+     * Returns whether the ripple decrement animation is currently underway.
+     *
+     * @return whether the ripple decrement animation is currently underway
+     */
+    public boolean inRippleDecrementAnimation() {
+        return inRippleDecrementAnimation.get();
+    }
+
+    /**
+     * Starts the ripple decrement animation.
+     */
+    @ForReadability
+    private void startRippleDecrementAnimation() {
+        Preconditions.checkState(!focused.get());
+
+        inRippleDecrementAnimation.set(true);
 
         CyderThreadRunner.submit(() -> {
-            while (borderLabelWidth.get() > 0 && !focused.get()) {
-                borderLabelWidth.getAndAdd(-rippleAnimationDelta);
-                borderLabel.setSize(borderLabelWidth.get(), borderThickness);
-                borderLabel.repaint();
+            while (rippleLabelWidth.get() > 0 && !focused.get() && shouldPerformFocusRipple.get()) {
+                rippleLabelWidth.getAndAdd(-rippleAnimationDelta);
+                rippleLabel.setSize(rippleLabelWidth.get(), rippleLabelThickness);
+                rippleLabel.repaint();
                 ThreadUtil.sleep(rippleAnimationTimeout);
             }
 
-            borderLabelWidth.set(0);
-            borderLabel.setSize(borderLabelWidth.get(), borderThickness);
-            borderLabel.repaint();
+            rippleLabelWidth.set((int) (getWidth() * minRippleLabelLengthPercentage));
+            rippleLabel.setSize(rippleLabelWidth.get(), rippleLabelThickness);
+            rippleLabel.repaint();
+
+            inRippleDecrementAnimation.set(false);
         }, rippleAnimationDecrementerThreadName);
     }
 
@@ -208,10 +448,16 @@ public class CyderModernTextField extends JTextField {
      */
     private void refreshRippleLabelBounds() {
         // Three because top and bottom of empty border and the bottom line border (this ripple border)
-        borderLabel.setBounds(0, getHeight() - 3 * borderThickness, borderLabelWidth.get(), borderThickness);
+        rippleLabel.setBounds(0, getHeight() - 3 * rippleLabelThickness,
+                rippleLabelWidth.get(), rippleLabelThickness);
     }
 
-    // todo call this when borderThickness changes
+    /**
+     * Refreshes the border meaning the border thickness will also be refreshed.
+     */
+    public void refreshBorder() {
+        setBorder(null);
+    }
 
     /**
      * Refreshes the thickness of the empty border.
@@ -220,6 +466,7 @@ public class CyderModernTextField extends JTextField {
      * @param ignored the border to be ignored
      */
     public void setBorder(Border ignored) {
-        super.setBorder(BorderFactory.createEmptyBorder(0, 0, borderThickness, 0));
+        super.setBorder(BorderFactory.createEmptyBorder(topLeftRightBorderInsets, topLeftRightBorderInsets,
+                rippleLabelThickness, topLeftRightBorderInsets));
     }
 }
