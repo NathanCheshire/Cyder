36f736adbc32bb94c772a599a61635ae81d87c1c
Nathan <nathan.vincent.2.718@gmail.com>
2022-12-25 20:20:36 -0600

    start github package unit tests

---
+++ src/test/kotlin/cyder/github/GithubCloneRepoLinkTest.kt
@@ -0,0 +1,48 @@
+package cyder.github
+
+import org.junit.jupiter.api.Test
+
+/**
+ * Tests for [GithubCloneRepoLink]s.
+ */
+class GithubCloneRepoLinkTest {
+    /**
+     * Tests for creation of github clone repo links.
+     */
+    @Test
+    fun testCreation() {
+
+    }
+
+    /**
+     * Tests for accessors, mutators, and other methods.
+     */
+    @Test
+    fun testMethods() {
+
+    }
+
+    /**
+     * Tests for the equals method.
+     */
+    @Test
+    fun testEquals() {
+
+    }
+
+    /**
+     * Tests for the hash code method.
+     */
+    @Test
+    fun testHashCode() {
+
+    }
+
+    /**
+     * Tests for the to string method.
+     */
+    @Test
+    fun testToString() {
+
+    }
+}
