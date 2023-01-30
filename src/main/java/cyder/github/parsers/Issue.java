995404663d5700fd65ed1aee3828687ced913205
Nathan <nathan.vincent.2.718@gmail.com>
2023-01-29 18:51:12 -0600

    add getters, setters, and javadoc to github issue parser class

--- src/main/java/cyder/github/parsers/Issue.java
+++ src/main/java/cyder/github/parsers/Issue.java
@@ -5,6 +5,7 @@ import cyder.logging.LogTag;
 import cyder.logging.Logger;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * A json serialization class for a GitHub repo issue.
@@ -57,7 +58,7 @@ public class Issue {
     public String nodeId;
 
     /**
-     * The sequential number of this issue
+     * The sequential number of this issue.
      */
     public int number;
 
@@ -134,7 +135,7 @@ public class Issue {
      * The reason for locking the issue.
      */
     @SerializedName("active_lock_reason")
-    public boolean activeLockReason;
+    public String activeLockReason;
 
     /**
      * The contents of the issue.
@@ -171,226 +172,506 @@ public class Issue {
         Logger.log(LogTag.OBJECT_CREATION, this);
     }
 
+    /**
+     * Returns the url for this issue.
+     *
+     * @return the url for this issue
+     */
     public String getUrl() {
         return url;
     }
 
+    /**
+     * Sets the url for this issue.
+     *
+     * @param url the url for this issue
+     */
     public void setUrl(String url) {
         this.url = url;
     }
 
+    /**
+     * Returns the url for the repository this issue was created for.
+     *
+     * @return the url for the repository this issue was created for
+     */
     public String getRepositoryUrl() {
         return repositoryUrl;
     }
 
+    /**
+     * Sets the url for the repository this issue was created for.
+     *
+     * @param repositoryUrl the url for the repository this issue was created for
+     */
     public void setRepositoryUrl(String repositoryUrl) {
         this.repositoryUrl = repositoryUrl;
     }
 
+    /**
+     * Returns the labels url for this issue.
+     *
+     * @return the labels url for this issue
+     */
     public String getLabelsUrl() {
         return labelsUrl;
     }
 
+    /**
+     * Sets the labels url for this issue.
+     *
+     * @param labelsUrl the labels url for this issue
+     */
     public void setLabelsUrl(String labelsUrl) {
         this.labelsUrl = labelsUrl;
     }
 
+    /**
+     * Returns the comments url for this issue.
+     *
+     * @return the comments url for this issue
+     */
     public String getCommentsUrl() {
         return commentsUrl;
     }
 
+    /**
+     * Sets the comments url for this issue.
+     *
+     * @param commentsUrl the comments url for this issue
+     */
     public void setCommentsUrl(String commentsUrl) {
         this.commentsUrl = commentsUrl;
     }
 
+    /**
+     * Returns the events url for this issue.
+     *
+     * @return the events url for this issue
+     */
     public String getEventsUrl() {
         return eventsUrl;
     }
 
+    /**
+     * Sets the events url for this issue.
+     *
+     * @param eventsUrl the events url for this issue
+     */
     public void setEventsUrl(String eventsUrl) {
         this.eventsUrl = eventsUrl;
     }
 
+    /**
+     * Returns the html url for this issue.
+     *
+     * @return the html url for this issue
+     */
     public String getHtmlUrl() {
         return htmlUrl;
     }
 
+    /**
+     * Sets the html url for this issue.
+     *
+     * @param htmlUrl the html url for this issue
+     */
     public void setHtmlUrl(String htmlUrl) {
         this.htmlUrl = htmlUrl;
     }
 
+    /**
+     * Returns the id of this issue.
+     *
+     * @return the id of this issue
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Sets the id of this issue.
+     *
+     * @param id the id of this issue
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Returns the node id of this issue.
+     *
+     * @return the node id of this issue
+     */
     public String getNodeId() {
         return nodeId;
     }
 
+    /**
+     * Sets the node id of this issue
+     *
+     * @param nodeId the node id of this issue
+     */
     public void setNodeId(String nodeId) {
         this.nodeId = nodeId;
     }
 
+    /**
+     * Returns the sequential number of this issue.
+     *
+     * @return the sequential number of this issue
+     */
     public int getNumber() {
         return number;
     }
 
+    /**
+     * Set the sequential number of this issue.
+     *
+     * @param number the sequential number of this issue
+     */
     public void setNumber(int number) {
         this.number = number;
     }
 
+    /**
+     * Returns the title of this issue.
+     *
+     * @return the title of this issue
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets the title of this issue.
+     *
+     * @param title the title of this issue
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Returns a user object.
+     *
+     * @return a user object
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * Sets a user object.
+     *
+     * @param user a user object
+     */
     public void setUser(User user) {
         this.user = user;
     }
 
+    /**
+     * Returns the associated with this issue.
+     *
+     * @return the associated with this issue
+     */
     public ArrayList<String> getLabels() {
         return labels;
     }
 
+    /**
+     * Sets the associated with this issue.
+     *
+     * @param labels the associated with this issue
+     */
     public void setLabels(ArrayList<String> labels) {
         this.labels = labels;
     }
 
+    /**
+     * Returns the state of this issue.
+     *
+     * @return the state of this issue
+     */
     public String getState() {
         return state;
     }
 
+    /**
+     * Sets the state of this issue.
+     *
+     * @param state the state of this issue
+     */
     public void setState(String state) {
         this.state = state;
     }
 
+    /**
+     * Returns whether this issue is currently locked.
+     *
+     * @return whether this issue is currently locked
+     */
     public boolean isLocked() {
         return locked;
     }
 
+    /**
+     * Sets whether this issue is currently locked.
+     *
+     * @param locked whether this issue is currently locked
+     */
     public void setLocked(boolean locked) {
         this.locked = locked;
     }
 
+    /**
+     * Returns the assignee for this issue.
+     *
+     * @return the assignee for this issue
+     */
     public User getAssignee() {
         return assignee;
     }
 
+    /**
+     * Sets the assignee for this issue.
+     *
+     * @param assignee the assignee for this issue
+     */
     public void setAssignee(User assignee) {
         this.assignee = assignee;
     }
 
+    /**
+     * Returns the assignees for this issue.
+     *
+     * @return the assignees for this issue
+     */
     public ArrayList<User> getAssignees() {
         return assignees;
     }
 
+    /**
+     * Sets the assignees for this issue.
+     *
+     * @param assignees the assignees for this issue
+     */
     public void setAssignees(ArrayList<User> assignees) {
         this.assignees = assignees;
     }
 
+    /**
+     * Returns whether this issue is a milestone
+     *
+     * @return whether this issue is a milestone
+     */
     public boolean isMilestone() {
         return milestone;
     }
 
+    /**
+     * Sets whether this issue is a milestone.
+     *
+     * @param milestone whether this issue is a milestone
+     */
     public void setMilestone(boolean milestone) {
         this.milestone = milestone;
     }
 
+    /**
+     * Returns the number of comments on this issue.
+     *
+     * @return the number of comments on this issue
+     */
     public int getComments() {
         return comments;
     }
 
+    /**
+     * Sets the number of comments on this issue.
+     *
+     * @param comments the number of comments on this issue
+     */
     public void setComments(int comments) {
         this.comments = comments;
     }
 
+    /**
+     * Returns the time at which this issue was created.
+     *
+     * @return the time at which this issue was created
+     */
     public String getCreatedAt() {
         return createdAt;
     }
 
+    /**
+     * Sets the time at which this issue was created.
+     *
+     * @param createdAt the time at which this issue was created
+     */
     public void setCreatedAt(String createdAt) {
         this.createdAt = createdAt;
     }
 
+    /**
+     * Returns the time at which this issue was last updated at.
+     *
+     * @return the time at which this issue was last updated at
+     */
     public String getUpdatedAt() {
         return updatedAt;
     }
 
+    /**
+     * Sets the time at which this issue was last updated at.
+     *
+     * @param updatedAt the time at which this issue was last updated at
+     */
     public void setUpdatedAt(String updatedAt) {
         this.updatedAt = updatedAt;
     }
 
+    /**
+     * Returns the time at which this issue was closed at.
+     *
+     * @return the time at which this issue was closed at
+     */
     public String getClosedAt() {
         return closedAt;
     }
 
+    /**
+     * Sets the time at which this issue was closed at.
+     *
+     * @param closedAt the time at which this issue was closed at
+     */
     public void setClosedAt(String closedAt) {
         this.closedAt = closedAt;
     }
 
+    /**
+     * Returns the association of the author of this issue.
+     *
+     * @return the association of the author of this issue
+     */
     public String getAuthorAssociation() {
         return authorAssociation;
     }
 
+    /**
+     * Sets the association of the author of this issue.
+     *
+     * @param authorAssociation the association of the author of this issue
+     */
     public void setAuthorAssociation(String authorAssociation) {
         this.authorAssociation = authorAssociation;
     }
 
-    public boolean isActiveLockReason() {
+    /**
+     * Returns the reason for locking the issue.
+     *
+     * @return the reason for locking the issue
+     */
+    public String isActiveLockReason() {
         return activeLockReason;
     }
 
-    public void setActiveLockReason(boolean activeLockReason) {
+    /**
+     * Sets the reason for locking the issue.
+     *
+     * @param activeLockReason the reason for locking the issue
+     */
+    public void setActiveLockReason(String activeLockReason) {
         this.activeLockReason = activeLockReason;
     }
 
+    /**
+     * Returns the contents of the issue.
+     *
+     * @return the contents of the issue
+     */
     public String getBody() {
         return body;
     }
 
+    /**
+     * Sets the contents of the issue.
+     *
+     * @param body the contents of the issue
+     */
     public void setBody(String body) {
         this.body = body;
     }
 
+    /**
+     * Returns the reactions to the issue.
+     *
+     * @return the reactions to the issue
+     */
     public Reaction getReactions() {
         return reactions;
     }
 
+    /**
+     * Sets the reactions to the issue.
+     *
+     * @param reactions the reactions to the issue
+     */
     public void setReactions(Reaction reactions) {
         this.reactions = reactions;
     }
 
+    /**
+     * Returns the url for the timeline of this issue.
+     *
+     * @return the url for the timeline of this issue
+     */
     public String getTimelineUrl() {
         return timelineUrl;
     }
 
+    /**
+     * Sets the url for the timeline of this issue.
+     *
+     * @param timelineUrl the url for the timeline of this issue
+     */
     public void setTimelineUrl(String timelineUrl) {
         this.timelineUrl = timelineUrl;
     }
 
+    /**
+     * Returns whether this issue was created via the GitHub app.
+     *
+     * @return whether this issue was created via the GitHub app
+     */
     public boolean isPerformedViaGithubApp() {
         return performedViaGithubApp;
     }
 
+    /**
+     * Sets whether this issue was created via the GitHub app.
+     *
+     * @param performedViaGithubApp whether this issue was created via the GitHub app
+     */
     public void setPerformedViaGithubApp(boolean performedViaGithubApp) {
         this.performedViaGithubApp = performedViaGithubApp;
     }
 
+    /**
+     * Returns the reason for the issue being in the current state.
+     *
+     * @return the reason for the issue being in the current state
+     */
     public String getStateReason() {
         return stateReason;
     }
 
+    /**
+     * Sets the reason for the issue being in the current state.
+     *
+     * @param stateReason the reason for the issue being in the current state
+     */
     public void setStateReason(String stateReason) {
         this.stateReason = stateReason;
     }
@@ -429,7 +710,7 @@ public class Issue {
                 && other.updatedAt.equals(updatedAt)
                 && other.closedAt.equals(closedAt)
                 && other.authorAssociation.equals(authorAssociation)
-                && other.activeLockReason == activeLockReason
+                && Objects.equals(other.activeLockReason, activeLockReason)
                 && other.body.equals(body)
                 && other.reactions.equals(reactions)
                 && other.timelineUrl.equals(timelineUrl)
@@ -465,7 +746,7 @@ public class Issue {
         ret = 31 * ret + updatedAt.hashCode();
         ret = 31 * ret + closedAt.hashCode();
         ret = 31 * ret + authorAssociation.hashCode();
-        ret = 31 * ret + Boolean.hashCode(activeLockReason);
+        ret = 31 * ret + activeLockReason.hashCode();
         ret = 31 * ret + body.hashCode();
         ret = 31 * ret + reactions.hashCode();
         ret = 31 * ret + timelineUrl.hashCode();
