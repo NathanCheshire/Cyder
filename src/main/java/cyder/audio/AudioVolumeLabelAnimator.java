dea045e42884cb56dcaf269f0e239eb77e2d6a61
Nathan <nathan.vincent.2.718@gmail.com>
2023-02-04 18:16:21 -0600

    finish working version of audio volume label font animation

--- src/main/java/cyder/audio/AudioVolumeLabelAnimator.java
+++ src/main/java/cyder/audio/AudioVolumeLabelAnimator.java
@@ -1,27 +1,19 @@
 package cyder.audio;
 
 import com.google.common.base.Preconditions;
-import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.ListenableFuture;
-import cyder.threads.CyderThreadFactory;
 import cyder.threads.CyderThreadRunner;
 import cyder.threads.ThreadUtil;
 
 import javax.swing.*;
 import java.awt.*;
 import java.time.Duration;
-import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * A class to control the visibility of the audio volume level label.
  */
 public class AudioVolumeLabelAnimator {
-    /**
-     * The total sleep time before setting the audio volume label to invisible.
-     */
-    private static final Duration maxLabelVisibleTime = Duration.ofSeconds(3);
-
     /**
      * The label to display the audio progress on when needed.
      */
@@ -38,22 +30,29 @@ public class AudioVolumeLabelAnimator {
         this.audioVolumePercentLabel = audioVolumePercentLabel;
     }
 
+    private final int maxVisibleTime = 3000;
+    private final AtomicLong millisUntilAnimateOut = new AtomicLong(maxVisibleTime);
+    private final int waitToAnimateOutSleepTime = 50;
+
     /**
      * The actions to invoke when the slider is changed.
      */
     public void onValueChanged() {
         if (!audioVolumePercentLabel.isVisible()) {
-            animatingOut.set(false);
+            if (animatingIn.get()) return;
             animateIn();
         } else {
-            if (waitToAnimateOutFuture != null) {
-                waitToAnimateOutFuture.cancel(true);
+            millisUntilAnimateOut.set(maxVisibleTime);
+            if (!animateOutWaiterRunning.get()) {
+                animateOutWaiterRunning.set(true);
+                CyderThreadRunner.submit(() -> {
+                    while (millisUntilAnimateOut.get() > 0) {
+                        millisUntilAnimateOut.getAndAdd(-waitToAnimateOutSleepTime);
+                        ThreadUtil.sleep(waitToAnimateOutSleepTime);
+                    }
+                    animateOut();
+                }, "Audio Volume Label Animator Animate Out Waiter");
             }
-
-            waitToAnimateOutFuture = Futures.submit(() -> {
-                ThreadUtil.sleep(maxLabelVisibleTime.toMillis());
-                animateOut();
-            }, Executors.newSingleThreadExecutor(new CyderThreadFactory("name")));
         }
     }
 
@@ -61,47 +60,54 @@ public class AudioVolumeLabelAnimator {
      * Kills this object.
      */
     public void kill() {
+        killed = true;
         animatingIn.set(false);
         animatingOut.set(false);
-        if (waitToAnimateOutFuture != null) waitToAnimateOutFuture.cancel(true);
     }
 
+    private boolean killed;
     private final AtomicBoolean animatingIn = new AtomicBoolean();
     private final AtomicBoolean animatingOut = new AtomicBoolean();
-    private ListenableFuture<Void> waitToAnimateOutFuture;
+    private final AtomicBoolean animateOutWaiterRunning = new AtomicBoolean();
+    private static final int animationStep = 2;
+    private static final Duration animationSleepTime = Duration.ofMillis(10);
 
-    private void animateIn() {
-        if (animatingIn.get()) return;
+    private synchronized void animateIn() {
+        if (animatingIn.get() || killed) return;
         animatingIn.set(true);
 
         CyderThreadRunner.submit(() -> {
             audioVolumePercentLabel.setVisible(true);
             Font font = audioVolumePercentLabel.getFont();
             int animateToFontSize = font.getSize();
-            for (int i = 0 ; i <= animateToFontSize ; i += 2) {
-                if (!animatingIn.get()) return;
-                audioVolumePercentLabel.setFont(font.deriveFont((float) i));
-                ThreadUtil.sleep(10);
+            for (int fontSize = 0 ; fontSize <= animateToFontSize ; fontSize += animationStep) {
+                if (!animatingIn.get() || killed) return;
+                audioVolumePercentLabel.setFont(font.deriveFont((float) fontSize));
+                ThreadUtil.sleep(animationSleepTime.toMillis());
             }
 
             audioVolumePercentLabel.setFont(font);
+            animatingIn.set(false);
+            millisUntilAnimateOut.set(maxVisibleTime);
         }, "Animate In Audio Volume Label");
     }
 
-    private void animateOut() {
-        if (animatingOut.get()) return;
+    private synchronized void animateOut() {
+        if (animatingOut.get() || killed) return;
         animatingOut.set(true);
 
         CyderThreadRunner.submit(() -> {
-            //            Font font = audioVolumePercentLabel.getFont();
-            //            for (int i = font.getSize() ; i >= 0 ; i--) {
-            //                if (!animatingOut.get()) return;
-            //                audioVolumePercentLabel.setFont(font.deriveFont((float) i));
-            //                ThreadUtil.sleep(50);
-            //            }
-            //
-            //            audioVolumePercentLabel.setVisible(false);
-            //            audioVolumePercentLabel.setFont(font);
+            Font font = audioVolumePercentLabel.getFont();
+            for (int fontSize = font.getSize() ; fontSize >= 0 ; fontSize -= animationStep) {
+                if (!animatingOut.get() || killed) return;
+                audioVolumePercentLabel.setFont(font.deriveFont((float) fontSize));
+                ThreadUtil.sleep(animationSleepTime.toMillis());
+            }
+
+            audioVolumePercentLabel.setVisible(false);
+            audioVolumePercentLabel.setFont(font);
+            animatingOut.set(false);
+            animateOutWaiterRunning.set(false);
         }, "Animate Out Audio Volume Label");
     }
 }
